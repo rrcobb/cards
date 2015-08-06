4994cc6cc38360b6dda954476b465733886a158b
rrcobb <rwcobbjr@gmail.com>
2015-08-05 19:11:35 -0700

    solved

--- cards.py
+++ cards.py
@@ -1,1 +1,17 @@
 # Write your function here!
+def everyone_sign(names):
+    result = {}
+
+    for name in names:
+        message = "I'll miss you! Thanks for the memories! Yours"
+        for friend in names:
+            # Don't sign your own name!
+            if friend == name:
+                # do nothing
+                pass
+            else:
+                message += ", " + friend
+        # add signature to a dictionary
+        result[name] = message
+
+    return result
